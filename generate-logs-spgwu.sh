6fed8730590f1416d76dfa44115a2bb71de306d4
Socratis Christakis <52699801+schristakis@users.noreply.github.com>
2023-11-23 14:11:26 +0100

    Update and rename generate-logs.sh to generate-logs-spgwu.sh

--- generate-logs-spgwu.sh
+++ generate-logs-spgwu.sh
@@ -1,6 +1,6 @@
 docker logs oai-amf >& amf_logs.txt
 docker logs oai-smf >& smf_logs.txt
-docker logs sdfabric-upf >& sdfabric-upf_logs.txt
+docker logs oai-spgwu >& spgwu_logs.txt
 docker logs oai-ausf >& ausf_logs.txt
 docker logs oai-udm >& udm_logs.txt
 docker logs oai-udr >& udr_logs.txt
